7e6d91d6a03706d303aa6d22129070342c067ae1
huyanping <hypxm@qq.com>
2015-04-18 22:22:10 +0800

    Rejected commit: Default

---
+++ simple/simple.c
@@ -0,0 +1,10 @@
+//
+// Created by lenovo on 2015/4/18.
+//
+
+#include "simple.h"
+
+int main(void){
+    printf('test');
+    return 0;
+}
---
+++ simple/simple.h
@@ -0,0 +1,8 @@
+//
+// Created by lenovo on 2015/4/18.
+//
+
+#ifndef C_TEST_CODE_SIMPLE_H
+#define C_TEST_CODE_SIMPLE_H
+
+#endif //C_TEST_CODE_SIMPLE_H
